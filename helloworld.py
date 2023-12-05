b045ddbc131ca981f334f600976afba6489efff4
kmilom <90485091+kmilom@users.noreply.github.com>
2023-12-04 23:40:37 -0500

    Primeros pasos con python

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+#Ejercicios sencillos con python
+"""
+print("Hello World")
+"""
+
+"""
+celsius = float(input("Ingrese la cantidad de grados celsius: "))
+farenheit = ((9/5)*celsius) + 32
+print(celsius, " grados en Farenheit: ", farenheit)
+"""
+
+"""
+n = int(input(("Ingrese un nú8mero entero: ")))
+if n % 2 == 0:
+    print(n, "es un número par.")
+else:
+    print(n, "es un número impar.")
+"""
