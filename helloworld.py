5be83b424846606e48c56130cdf1dcc47dbcd138
kmilom <90485091+kmilom@users.noreply.github.com>
2023-12-06 23:12:16 -0500

    ejercicios sencillos

--- helloworld.py
+++ helloworld.py
@@ -16,3 +16,38 @@ if n % 2 == 0:
 else:
     print(n, "es un número impar.")
 """
+
+"""
+i = 0
+while i < 5:
+    j = 0
+    while j <= i:
+        print("*", end="")
+        j += 1
+    print("")
+    i += 1
+
+for i in range(5):
+    j = 0
+    while j <= i:
+        print("*", end="")
+        j += 1
+    print()
+"""
+"""
+top = 10
+for i in range(top):
+    for j in range(top * 2):
+        if j == top or (j >= top - i and j <= top + i):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+for i in range(top, -1, -1):
+    for j in range(top * 2 + 1):
+        if j == top or (j >= top - i and j <= top + i):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+"""
